ccce5f24d6e4ce87787f6e3466e5e5d5d2659269
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-09 16:31:50 -0400

    Add recipes to json file and render each recipe to page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,17 +3,31 @@ import React from "react";
 import { CardGrid } from "../../Elements/CardGrid/";
 import RecipeCard from "../../Components/RecipeCard/";
 
+import recipes from "../../assets/recipes.json";
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: recipes,
+    };
+  }
+
   render() {
-    return (
-      <CardGrid>
-        <RecipeCard name="Granola" />
-        <RecipeCard name="Granola" />
-        <RecipeCard name="Granola" />
-        <RecipeCard name="Granola" />
-      </CardGrid>
-    );
+    const getRecipes = this.state.recipes.map((recipe) => {
+      const { name, ingredients, method } = recipe;
+      return (
+        <RecipeCard
+          key={name}
+          name={name}
+          ingredients={ingredients}
+          method={method}
+        />
+      );
+    });
+
+    return <CardGrid>{getRecipes}</CardGrid>;
   }
 }
-
 export default Home;
