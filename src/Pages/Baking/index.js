29d3aeb6c9c9ae0bb2172790c4ebf74fa7fdccfa
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-10 10:53:12 -0400

    Add specific recipes to Baking page

--- src/Pages/Baking/index.js
+++ src/Pages/Baking/index.js
@@ -1,12 +1,43 @@
 import React from "react";
 
+import { CardGrid } from "../../Elements/CardGrid/";
+import RecipeCard from "../../Components/RecipeCard/";
+
+import recipes from "../../assets/recipes.json";
+
 class Baking extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: recipes,
+      category: "baking",
+    };
+  }
+
+  getBakingRecipes = () => {
+    const bakingRecipes = this.state.recipes.filter((recipe) => {
+      const category = this.state.category;
+      return recipe.category === category;
+    });
+    return bakingRecipes;
+  };
+
   render() {
-    return (
-      <div>
-        <p>content of this baking page</p>
-      </div>
-    );
+    const bakingRecipes = this.getBakingRecipes();
+    const displayBakingRecipes = bakingRecipes.map((recipe) => {
+      const { name, ingredients, method } = recipe;
+      return (
+        <RecipeCard
+          key={name}
+          name={name}
+          ingredients={ingredients}
+          method={method}
+        />
+      );
+    });
+
+    return <CardGrid>{displayBakingRecipes}</CardGrid>;
   }
 }
 
