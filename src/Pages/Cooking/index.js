00ae6f2c03186637fc85bde7a2b3b8626ea34b5c
Rebekah DeJong <rebekahdejong@Rebekahs-MacBook-Air.local>
2020-08-10 10:37:33 -0400

    Add specific recipes to Cooking page

--- src/Pages/Cooking/index.js
+++ src/Pages/Cooking/index.js
@@ -1,13 +1,43 @@
 import React from "react";
 
+import { CardGrid } from "../../Elements/CardGrid/";
+import RecipeCard from "../../Components/RecipeCard/";
+
+import recipes from "../../assets/recipes.json";
+
 class Cooking extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: recipes,
+      category: "cooking",
+    };
+  }
+
+  getCookingRecipes = () => {
+    const cookingRecipes = this.state.recipes.filter((recipe) => {
+      const category = this.state.category;
+      return recipe.category === category;
+    });
+    return cookingRecipes;
+  };
+
   render() {
-    return (
-      <div>
-        <p>content of this cooking page</p>
-      </div>
-    );
+    const cookingRecipes = this.getCookingRecipes();
+    const displayCookingRecipes = cookingRecipes.map((recipe) => {
+      const { name, ingredients, method } = recipe;
+      return (
+        <RecipeCard
+          key={name}
+          name={name}
+          ingredients={ingredients}
+          method={method}
+        />
+      );
+    });
+
+    return <CardGrid>{displayCookingRecipes}</CardGrid>;
   }
 }
-
 export default Cooking;
